ec62ed27f381f2cd17cdbe7e83aa7ef1562755bd
Mark Wayne Menorca <marcuwynu23@gmail.com>
2026-05-13 09:34:01 +0800

    refactor(terraform): move outputs to separate file
    - Remove output blocks from variables.tf
    - Outputs will be defined in dedicated outputs.tf file
    - Improves separation of concerns and follows Terraform best practices

--- variables.tf
+++ variables.tf
@@ -16,14 +16,3 @@ variable "database_name" {
   type        = string
   default     = "my-database"
 }
-
-# Outputs
-output "d1_database_id" {
-  description = "UUID of the D1 database"
-  value       = cloudflare_d1_database.main.id
-}
-
-output "d1_database_name" {
-  description = "Name of the D1 database"
-  value       = cloudflare_d1_database.main.name
-}
